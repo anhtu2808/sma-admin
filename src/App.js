772b9128585379772313827580aac6144d599554
Trinh Nhan <147188783+trinhhnhann@users.noreply.github.com>
2026-03-15 01:39:47 +0700

    noti update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import { RouterProvider } from 'react-router-dom';
 import { routes } from '@/routes';
 import './App.css';
+import { useNotificationSocket } from '@/hooks/useNotificationSocket';
+import { Toaster } from "react-hot-toast";
 
 function App() {
-  return <RouterProvider router={routes} />;
+  useNotificationSocket();
+  return (
+    <>
+      <Toaster
+        position="bottom-right"
+        toastOptions={{
+          style: {
+            background: "transparent",
+            boxShadow: "none"
+          }
+        }}
+      />
+      <RouterProvider router={routes} />;
+    </>
+  );
 }
 
 export default App;
--- src/hooks/useNotificationSocket.js
+++ src/hooks/useNotificationSocket.js
@@ -4,10 +4,32 @@ import SockJS from 'sockjs-client';
 import { Client } from '@stomp/stompjs';
 import { api } from '@/apis/baseApi';
 import { setRealtimePreview } from '../pages/notification/components/notification-slice';
+import NotificationToast from '@/components/NotificationToast';
+import toast from "react-hot-toast";
 
 export const useNotificationSocket = () => {
     const dispatch = useDispatch();
+    const getIcon = (type) => {
+        switch (type) {
+            case 'SYSTEM':
+                return 'error_outline';
 
+            case 'PAYMENT_SUCCESS':
+                return 'check_circle';
+
+            case 'PAYMENT_FAILURE':
+                return 'payments';
+
+            case 'APPLICATION_STATUS':
+                return 'contact_page';
+
+            case 'FLAGGED_JOB':
+                return 'work_outline';
+
+            default:
+                return 'notifications';
+        }
+    };
     useEffect(() => {
         const token = localStorage.getItem('accessToken');
         if (!token) return;
@@ -27,7 +49,19 @@ export const useNotificationSocket = () => {
                 client.subscribe('/user/queue/notifications', (message) => {
                     console.log("🔥 WS RECEIVED:", message.body);
                     const newNoti = JSON.parse(message.body);
-                    dispatch(setRealtimePreview(newNoti));
+                    if (!newNoti?.title && !newNoti?.message) {
+                        return;
+                    }
+                    const icon = getIcon(newNoti.notificationType);
+
+                    toast.custom((t) => (
+                        <NotificationToast
+                            t={t}
+                            icon={icon}
+                            title={newNoti.title}
+                            message={newNoti.message}
+                        />
+                    ), { duration: 5000 });
                     dispatch(api.util.invalidateTags(['Notifications']));
                 });
             },
